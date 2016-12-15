e842dd71856f2629105e70a33fba3cd21e3d0430
Marco Hyyryläinen <marco@wheresmar.co>
2016-12-15 12:36:23 +0100

    First version of the plugin added.

---
+++ README.txt
@@ -0,0 +1,24 @@
+=== TuffTuffTime ===
+Contributors: oddalice, wheresmarco
+Tags: train, timetable, sj, sweden, trafiklab, trafikverket
+Requires at least: 4.7.0
+Tested up to: 4.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds Access-Control-Allow-Origin: * to all REST endpoints.
+
+== Description ==
+
+Adds Access-Control-Allow-Origin: * to all REST endpoints.
+
+== Installation ==
+
+1. Upload the folder `rest-allow-origin` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+
+= 1.0 =
+* First release.
---
+++ rest-allow-origin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  Plugin Name: REST Allow Origin
+  Plugin URI: https://oddalice.se
+  Description: Adds Access-Control-Allow-Origin: * to all REST endpoints.
+  Version: 1.0
+  Author: Odd Alice
+  Author URI: http://oddalice.com
+  Text Domain: oa-rest-allow-origin
+  Domain Path: /languages
+*/
+
+add_action( 'rest_api_init', function() {
+  // Remove the old cors headers
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+  // Readd the cors headers with allow origin *
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
+
+		return $value;
+	});
+}, 15 );
